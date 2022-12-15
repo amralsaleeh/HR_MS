1e76e026d14f25cf20008a849aa8bb3249f30f20
amralsaleeh <3mr.alsaelh@gmail.com>
2022-12-15 12:16:58 +0300

    Main

--- app/Imports/ImportAttendance.php
+++ app/Imports/ImportAttendance.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Attendee;
+use Illuminate\Support\Carbon;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 
@@ -12,14 +13,17 @@ class ImportAttendance implements ToModel, WithStartRow
 
     public function model(array $column)
     {
-        return new Attendee([
-            'employeeId' => $column[0],
-            'logDate' => $column[1],
-            'logTime' => $column[2],
-            'login' => $column[3],
-            'logout' => $column[4],
-            'duration' => $column[5],
-        ]);
+        if(Carbon::parse($column[1])->isWeekday())
+        {
+            return new Attendee([
+                'employeeId' => $column[0],
+                'logDate' => $column[1],
+                'logTime' => $column[2],
+                // 'login' => $column[3],
+                // 'logout' => $column[4],
+                // 'duration' => $column[5],
+            ]);
+        }
     }
 
     public function startRow(): int
